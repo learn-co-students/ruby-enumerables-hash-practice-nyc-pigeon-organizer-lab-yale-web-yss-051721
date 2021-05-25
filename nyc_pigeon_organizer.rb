1e69bb2fdfd86a9e6ee4e70c3fe1c0e4f8e66651
Bill Feng <bill.feng@yale.edu>
2021-05-25 00:37:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newHash={}
+  data.each{
+    |attributeName, attributeValue|
+    attributeValue.each{
+      |attribute_val, names|
+      names.each{|name|
+      if(newHash[name]==nil)
+        newHash[name]={}
+      end
+      if(newHash[name][attributeName]==nil)
+        newHash[name][attributeName]=[]
+      end
+      newHash[name][attributeName]<< attribute_val.to_s
+      }
+    }
+  }
+  newHash
 end
