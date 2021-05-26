d16bb17c60a3f470d37549ddac944add4b1f583c
clean-loop-a64b <dsjong2002@yahoo.com>
2021-05-26 08:50:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  ans = {}
+  data.each do |general,pairs|
+    pairs.each do |spec,names|
+      names.each do |name|
+        #puts "#{name} #{general} #{spec}"
+        ans[name] = {} if !ans[name]
+        if ans[name][general]
+          #puts "adding #{spec} to #{general} of #{name}"
+          ans[name][general] += [spec.to_s]
+        else
+          ans[name][general] = [spec.to_s]
+        end
+      end
+    end
+  end
+  ans
 end
