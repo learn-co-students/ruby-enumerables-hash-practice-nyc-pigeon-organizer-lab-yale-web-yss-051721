9244c09bfd7dbf498595b67d670c3f6f25473870
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-27 15:31:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  final_hash = data.each_with_object({}) do |(key, val), fin_arr|
+    val.each do |inner_key, name_arr|
+      name_arr.each do |name|
+        if !fin_arr[name]
+          fin_arr[name] = {}
+        end
+        if !fin_arr[name][key]
+          fin_arr[name][key] = []
+        end
+        fin_arr[name][key] << inner_key.to_s
+        
+      end 
+    end 
+  end
 end
